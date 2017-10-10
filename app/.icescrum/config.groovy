5998901a4d4d2a8f9977e4eefe24ee41f7205062
apedroed <apedroed88@gmail.com>
2017-10-09 23:46:32 -0300

    export port.

--- app/.icescrum/config.groovy
+++ app/.icescrum/config.groovy
@@ -1,1 +1,16 @@
-dataSource.url = 'jdbc:h2:/root/h2/prodDb'
+dataSource.driverClassName="org.postgresql.Driver"
+dataSource.password="Silver99"
+dataSource.url="jdbc:postgresql://db:5432/icescrum"
+dataSource.username="postgres"
+icescrum.baseDir="/root/icescrum"
+icescrum.debug.enable=false
+icescrum.gravatar.enable=true
+icescrum.log.dir="/root/logs/"
+icescrum.project.creation.enable=true
+icescrum.project.export.enable=true
+icescrum.project.import.enable=true
+icescrum.project.private.default=true
+icescrum.project.private.enable=true
+icescrum.registration.enable=true
+icescrum.reportUsage.enable=true
+icescrum.serverURL="http://localhost:8080/icescrum"
